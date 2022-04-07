7eb0ae5dbf91d6e587929b969c79cb7e5d44e717
Abir0248 <abir15-6789@diu.edu.bd>
2022-04-08 02:44:41 +0600

    event handlers added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { getAuth } from "firebase/auth";
-
-import { app } from "./firebase.init";
+import app from "./firebase.init";
 
 const auth = getAuth(app);
 
 function App() {
+  const handleEmailChange = (event) => {
+    console.log(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <div className="App">
-      <h1>hello i am firebase </h1>
+      <h1>Hello i am firebase </h1>
+
+      <form action="">
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name=""
+          id=""
+          placeholder="email"
+        />
+        <br />
+        <input
+          onChange={handlePasswordChange}
+          type="password"
+          name=""
+          id=""
+          placeholder="password"
+        />
+      </form>
     </div>
   );
 }
