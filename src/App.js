9cc2ab58f246531822a4b5b2cdd858cf3750b636
Abir0248 <abir15-6789@diu.edu.bd>
2022-04-08 03:04:08 +0600

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { getAuth } from "firebase/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
 import app from "./firebase.init";
 
 const auth = getAuth(app);
 
 function App() {
-  const handleEmailChange = (event) => {
+  const handleEmailBlur = (event) => {
     console.log(event.target.value);
   };
-  const handlePasswordChange = (event) => {
+  const handlePasswordBlur = (event) => {
     console.log(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    console.log("form submitted");
+    event.preventDefault();
   };
   return (
     <div className="App">
       <h1>Hello i am firebase </h1>
 
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
-          onChange={handleEmailChange}
+          onBlur={handleEmailBlur}
           type="email"
           name=""
           id=""
@@ -27,12 +32,14 @@ function App() {
         />
         <br />
         <input
-          onChange={handlePasswordChange}
+          onBlur={handlePasswordBlur}
           type="password"
           name=""
           id=""
           placeholder="password"
         />
+        <br />
+        <input type="submit" value="Log In" />
       </form>
     </div>
   );
