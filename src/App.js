fd31a0bfe9ce4d2fc3410caae489982e9744fb9d
Abir0248 <abir15-6789@diu.edu.bd>
2022-04-09 01:14:07 +0600

    bootstrap form added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import { getAuth } from "firebase/auth";
 import "bootstrap/dist/css/bootstrap.min.css";
 import app from "./firebase.init";
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 const auth = getAuth(app);
 
@@ -20,9 +22,7 @@ function App() {
   };
   return (
     <div className="App">
-      <h1>Hello i am firebase </h1>
-
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <input
           onBlur={handleEmailBlur}
           type="email"
@@ -40,7 +40,36 @@ function App() {
         />
         <br />
         <input type="submit" value="Log In" />
-      </form>
+      </form> */}
+      <div className="registration w-50 mx-auto mt-5">
+        <h1 className="text-primary">Please Register</h1>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              onBlur={handleEmailBlur}
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onBlur={handlePasswordBlur}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
